7f30eff6685f3189303fc0438307e5a991b85147
itsdhanudev <dhanu2872009@gmail.com>
2025-10-17 16:49:41 +0530

    updated and changed the easer structure

--- Source/ITs Ease/Files/Scripts/Test.java
+++ Source/ITs Ease/Files/Scripts/Test.java
@@ -1,8 +1,16 @@
 /* @Author Dhanu */
-public float time, duration, b;
-public Vector3 start = new Vector3(), end = new Vector3();
-public Text3D text;
+public enum Mode {
+  P,
+  R,
+  S
+}
 
+public Mode mode = Mode.P;
+public float time, duration, b;
+public Vector3 start = new Vector3();
+public Vector3 end = new Vector3();
+public Text3D text;
+public EasingTypes type;
 /// Run only once
 void start() {}
 
@@ -10,20 +18,21 @@ void start() {}
 void repeat() {
   if (time < duration) {
     time += Math.bySecond();
-  } 
+  }
 
   float t = Mathf.normalize(time / (duration), 0f, 1f);
-
-  // myTransform.getPosition().setX(lerp(start.getX(), end.getX(), t));
-  // myTransform.getPosition().setY(lerp(start.getY(), end.getY(), t));
-  // myTransform.getPosition().setZ(lerp(start.getZ(), end.getZ(), t));
-  myTransform.getPosition().setX(Easer.easeOutCirc(start.getX(), end.getX(),t));
+  switch (mode) {
+    case P:
+      myTransform.setPosition(Easer.ease(start.getX(), end.getX(), t, type), Easer.ease(start.getY(), end.getY(), t, type), Easer.ease(start.getZ(), end.getZ(), t, type));
+      break;
+    case R:
+      myTransform.getRotation().setFromEuler(Easer.ease(start.getX(), end.getX(), t, type), Easer.ease(start.getY(), end.getY(), t, type), Easer.ease(start.getZ(), end.getZ(), t, type));
+      break;
+    case S:
+      myTransform.setScale(Easer.ease(start.getX(), end.getX(), t, type), Easer.ease(start.getY(), end.getY(), t, type), Easer.ease(start.getZ(), end.getZ(), t, type));
+      break;
+    default:
+  } 
 
   text.setText(new String().format("%.2f", time));
-}
-
-public static float lerp(float start, float end, float t) {
-  if (t < 0f) t = 0f;
-  if (t > 1f) t = 1f;
-  return start + (end - start) * t;
 }
