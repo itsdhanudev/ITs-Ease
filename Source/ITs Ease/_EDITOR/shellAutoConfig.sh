d2bb6d0a3ae659b7a666ba5189b7dd063a62eb04
itsdhanudev <dhanu2872009@gmail.com>
2025-10-21 08:22:37 +0530

    minor fixes

--- Source/ITs Ease/_EDITOR/shellAutoConfig.sh
+++
@@ -1,2 +0,0 @@
-# SHELL AUTO CONFIG FILE
-# All commands here will execute automatically everytime a new shell is open in terminal.
--- Source/ITs Ease/_EDITOR/shellAutoRun.sh
+++
@@ -1,2 +0,0 @@
-# SHELL AUTO RUN
-# all commands here will execute automatically on a background and hidden shell, everytime the project is open
