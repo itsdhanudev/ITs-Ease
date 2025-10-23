c87d0be355ec315a207d5d8ac4cec55ab2138a44
itsdhanudev <dhanu2872009@gmail.com>
2025-10-23 09:41:29 +0530

    update

--- Demos/UIBased Demo/Files/Scripts/RectTweener.java
+++ Demos/UIBased Demo/Files/Scripts/RectTweener.java
@@ -10,16 +10,38 @@ import java.nio.*;
 import java.util.HashMap;
 import java.util.Map;
 
-/* @Author Dhanu */ 
-public class RectTweener extends Component{
-    // we be updated later
-   /// Run only once
-   void start() {
-       
-   }
-   
-   /// Repeat every frame
-   void repeat() {
-       
-   }
+/* @Author Dhanu */
+public class RectTweener extends Component {
+  public EasingTypes easeType = EasingTypes.Linear;
+
+  @AutoWired @Hide public UIRect rect;
+
+  public Vector2 start = new Vector2();
+  public Vector2 end = new Vector2();
+
+  public float time;
+  public float duration;
+
+  public boolean useCurve;
+  public Curve curve = new Curve();
+
+  /// Run only once
+  void start() {}
+
+  /// Repeat every frame
+  void repeat() {
+
+    if (time < duration) {
+      time += Math.bySecond();
+    } 
+    
+    float t = Mathf.normalize(time / duration, 0f, 1f);
+    if (!useCurve) setRectPos(Easer.easeVec(start, end, t, easeType), rect);
+    else setRectPos(Easer.easeVec(start, end, t, curve), rect);
+  }
+
+  public void setRectPos(Vector2 vec, UIRect _rect) {
+    _rect.setJRPositionX((int) vec.getX());
+    _rect.setJRPositionY((int) vec.getY());
+  }
 }
